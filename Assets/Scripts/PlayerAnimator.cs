9874eed9319368fb79f26ced17fcd1b3305e7fa0
emilsenkel <emil.senkel@gmail.com>
2025-08-21 15:17:13 +0200

    Update PlayerAnimator.cs

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -2,24 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAnimator : MonoBehaviour {
-
-
+public class PlayerAnimator : MonoBehaviour
+{
     private const string IS_WALKING = "IsWalking";
 
-
-    [SerializeField] private Player player;
-
+    private Player player;
 
     private Animator animator;
 
-
-    private void Awake() {
+    private void Awake()
+    {
         animator = GetComponent<Animator>();
+        player = GetComponent<Player>();
     }
 
-    private void Update() {
+    private void Update()
+    {
         animator.SetBool(IS_WALKING, player.IsWalking());
     }
-
 }
