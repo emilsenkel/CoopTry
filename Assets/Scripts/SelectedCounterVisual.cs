534b898d87e44259f1c2b9dc5801906055f99f98
emilsenkel <emil.senkel@gmail.com>
2025-08-21 15:17:00 +0200

    Update SelectedCounterVisual.cs

--- Assets/Scripts/SelectedCounterVisual.cs
+++ Assets/Scripts/SelectedCounterVisual.cs
@@ -1,36 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class SelectedCounterVisual : MonoBehaviour {
-
-
+public class SelectedCounterVisual : MonoBehaviour
+{
     [SerializeField] private BaseCounter baseCounter;
     [SerializeField] private GameObject[] visualGameObjectArray;
 
-
-    private void Start() {
-        Player.Instance.OnSelectedCounterChanged += Player_OnSelectedCounterChanged;
+    private void Start()
+    {
+        // Subscribe to all players
+        foreach (var player in FindObjectsOfType<Player>())
+        {
+            player.OnSelectedCounterChanged += Player_OnSelectedCounterChanged;
+        }
+        PlayerInputManager.instance.onPlayerJoined += (PlayerInput pi) =>
+        {
+            var player = pi.GetComponent<Player>();
+            player.OnSelectedCounterChanged += Player_OnSelectedCounterChanged;
+        };
     }
 
-    private void Player_OnSelectedCounterChanged(object sender, Player.OnSelectedCounterChangedEventArgs e) {
-        if (e.selectedCounter == baseCounter) {
+    private void Player_OnSelectedCounterChanged(object sender, Player.OnSelectedCounterChangedEventArgs e)
+    {
+        if (e.selectedCounter == baseCounter)
+        {
             Show();
-        } else {
+        }
+        else
+        {
             Hide();
         }
     }
 
-    private void Show() {
-        foreach (GameObject visualGameObject in visualGameObjectArray) {
+    private void Show()
+    {
+        foreach (GameObject visualGameObject in visualGameObjectArray)
+        {
             visualGameObject.SetActive(true);
         }
     }
 
-    private void Hide() {
-        foreach (GameObject visualGameObject in visualGameObjectArray) {
+    private void Hide()
+    {
+        foreach (GameObject visualGameObject in visualGameObjectArray)
+        {
             visualGameObject.SetActive(false);
         }
     }
-
 }
